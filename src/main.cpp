2ba63387be556c7eb42390a9eed817e67aadc732
Jae-Hyuck Park <jaehyuck0103@gmail.com>
2021-08-25 15:44:55 +0900

    save cam by opencv.

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,57 @@
 #include "pandar/pandar_lidar_receiver.h"
 #include "pandar/pandar_utils.h"
 
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <tbb/concurrent_queue.h>
+#include <toml.hpp>
+
 #include <chrono>
 #include <filesystem>
 #include <iostream>
 #include <memory>
-#include <tbb/concurrent_queue.h>
-#include <toml.hpp>
 
 namespace fs = std::filesystem;
+namespace chrono = std::chrono;
 using std::chrono::system_clock;
 using LidarQueue =
     tbb::concurrent_bounded_queue<std::tuple<int, std::vector<uint8_t>, time_point<system_clock>>>;
+using namespace std::chrono_literals;
+
+void captureCamera(int cam_id, fs::path save_root) {
+
+    save_root /= ("cam0" + std::to_string(cam_id));
+    fs::create_directories(save_root);
+
+#if 1
+    std::string video_path = "videos/" + std::to_string(cam_id) + ".mkv";
+    cv::VideoCapture cap{video_path};
+#else
+    cv::VideoCapture cap{cam_id};
+#endif
+
+    if (cap.isOpened()) {
+        std::cout << "cam" << cam_id << " open\n";
+    } else {
+        std::cout << "cam" << cam_id << " fail to open\n";
+        return;
+    }
+
+    cv::Mat abc;
+    while (cap.read(abc)) {
+
+        auto timestamp_us =
+            chrono::duration_cast<chrono::microseconds>(system_clock::now().time_since_epoch())
+                .count();
+
+        const fs::path jpg_save_path = save_root / (std::to_string(timestamp_us) + ".jpg");
+        // std::cout << abc.cols << "\n";
+        cv::imwrite(jpg_save_path, abc, {cv::IMWRITE_JPEG_QUALITY, 100});
+        // std::this_thread::sleep_for(5ms);
+    }
+
+    std::cout << "cam" << cam_id << " exit\n";
+}
 
 fs::path parseSaveRoot(std::string str_root) {
     // Expand Home Directory
@@ -37,23 +77,6 @@ fs::path parseSaveRoot(std::string str_root) {
     return root;
 }
 
-void lidarCallback(
-    LidarQueue &lidar_queue,
-    int lidar_id,
-    const std::vector<uint8_t> &frame_buffer,
-    time_point<system_clock> &timestamp) {
-
-    /*
-    std::cout << "timestamp: "
-              << std::chrono::duration_cast<std::chrono::microseconds>(
-                     timestamp.time_since_epoch())
-                     .count()
-              << "\n";
-    */
-
-    lidar_queue.try_push({lidar_id, frame_buffer, timestamp});
-}
-
 int main() {
 
     std::cout << "\n\n\n\n\n";
@@ -66,6 +89,7 @@ int main() {
 
     // Init lidar receviers
     LidarQueue lidar_queue;
+    lidar_queue.set_capacity(5);
 
     std::vector<PandarConfig> pandar_cfgs;
     std::vector<std::unique_ptr<PandarLidarReceiver>> pandar_lidar_receivers;
@@ -97,8 +121,8 @@ int main() {
             .emplace_back(std::make_unique<PandarLidarReceiver>(
                 lidar_ip,
                 recv_port,
-                [&lidar_queue, lidar_id{pandar_cfgs.size() - 1}](auto a, auto b) {
-                    lidarCallback(lidar_queue, lidar_id, a, b);
+                [&lidar_queue, lidar_id{pandar_cfgs.size() - 1}](const auto &a, const auto &b) {
+                    lidar_queue.try_push({lidar_id, a, b});
                 },
                 pandar_cfgs.back()))
             ->start();
@@ -113,6 +137,18 @@ int main() {
         const fs::path lidar_save_dir = save_root / ("Lidar0" + std::to_string(lidar_id));
         fs::create_directories(lidar_save_dir);
     }
+
+    // Launch camera threads
+    std::vector<std::thread> camera_threads;
+    for (int cam_id = 0; cam_id < 8; ++cam_id) {
+        camera_threads.emplace_back([=]() { captureCamera(cam_id, save_root); });
+    }
+
+    /*
+    for (int cam_id = 0; cam_id < 8; ++cam_id) {
+        camera_threads[cam_id].join();
+    }
+    */
 
     // Lidar Receive Handling
     std::tuple<int, std::vector<uint8_t>, time_point<system_clock>> ret;
